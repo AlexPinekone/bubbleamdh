15d68cf512d3bcb6560fd175133f1b93b11b7397
AlexPinekone <a335770@alumnos.uaslp.mx>
2025-01-21 17:02:12 -0600

    Bubbles!
    En efecto burbujas... bugueadas

--- Nodos/Jugador/player.gd
+++ Nodos/Jugador/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
+var bubble = preload("res://Nodos/bubble.tscn")
+
 const SPEED = 140
+var bubble_speed = 6
+var bubble_time = 0.5
 
 var input_direction: get = _get_input_direction
 var sprite_direction= "Down": get = _get_sprite_direction
@@ -9,6 +13,9 @@ var sprite_direction= "Down": get = _get_sprite_direction
 
 func _physics_process(_delta):
 	velocity = input_direction * SPEED
+	
+	if Input.is_action_just_pressed("blow"):
+		blow()
 	move_and_slide()
 	
 	set_animation("walk")
@@ -37,3 +44,21 @@ func _get_sprite_direction():
 		Vector2.DOWN:
 			sprite_direction = "Down"
 	return sprite_direction
+
+func blow():
+	#$AnimatedSprite.play("blow")
+	
+	var bubble_ins = bubble.instantiate()
+	if sprite_direction == "Right":
+		bubble_ins.speedX = bubble_speed * ((velocity.x / SPEED) if velocity.x != 0 || velocity.y !=0 else 1)
+	else:
+		bubble_ins.speedX = -bubble_speed * ((-velocity.x / SPEED) if velocity.x != 0 || velocity.y !=0 else 1)
+		
+	if sprite_direction == "Down":
+		bubble_ins.speedY = bubble_speed * ((velocity.y / SPEED) if velocity.x != 0 || velocity.y !=0 else 1)
+	else:
+		bubble_ins.speedY = -bubble_speed * ((-velocity.y / SPEED) if velocity.x != 0 || velocity.y !=0 else 1)
+		
+	bubble_ins.time_on_air = bubble_time
+	bubble_ins.position = position
+	get_parent().add_child(bubble_ins)
